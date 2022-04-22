61ef9ff526cca300fb7426ee62e5b152ee654f27
Jitendrasingh <jitendrasinghrao9587@gmail.com>
2022-04-22 16:47:13 +0530

    react side bar with dropdown

---
+++ react-sidebar-dropdown/src/pages/Message.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Message = () => {
+  return (
+    <div>Message</div>
+  )
+}
+
+
+export const Message1 = () => {
+  return (
+    <div>message 1</div>
+  )
+}
+
+
+export const Message2 = () => {
+  return (
+    <div>Message2</div>
+  )
+}
